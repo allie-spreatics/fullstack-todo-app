bc152024e426508c03fb770ddb1a327275e3d1d4
jinhyeong830 <shg02387@naver.com>
2024-04-05 17:19:33 +0900

    feat: [GET /todos],  [POST /todo] api 완료

---
+++ server/routes/user.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controller/CMain");
+// GET /api-server/user
+router.get("/", controller.getUser);
+module.exports = router;
